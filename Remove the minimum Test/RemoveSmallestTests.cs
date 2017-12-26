f97e16b069533a30230556263b46616b191b2e9b
Mason Cheng <masonsanchen@>
2017-12-26 11:26:54 +0800

    add projects Remove the minimum & test

---
+++ Remove the minimum Test/RemoveSmallestTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Remove_the_minimum;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Remove_the_minimum_Test
+{
+    [TestClass]
+    public class RemoveSmallestTests
+    {
+        private static void Tester(List<int> argument, List<int> expectedResult)
+        {
+            CollectionAssert.AreEqual(expectedResult, Remover.RemoveSmallest(argument));
+        }
+        [Test]
+        public static void BasicTests()
+        {
+            Tester(new List<int> { 1, 2, 3, 4, 5 }, new List<int> { 2, 3, 4, 5 });
+            Tester(new List<int> { 5, 3, 2, 1, 4 }, new List<int> { 5, 3, 2, 4 });
+            Tester(new List<int> { 1, 2, 3, 1, 1 }, new List<int> { 2, 3, 1, 1 });
+            Tester(new List<int>(), new List<int>());
+        }
+    }
+}
