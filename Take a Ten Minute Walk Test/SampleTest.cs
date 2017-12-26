f28b7c2ed7fb04e1dbc0f3a08a189ee931467175
Mason Cheng <masonsanchen@>
2017-12-26 13:50:42 +0800

    add Take a Ten Minute Walk and test project

---
+++ Take a Ten Minute Walk Test/SampleTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Take_a_Ten_Minute_Walk;
+
+namespace Take_a_Ten_Minute_Walk_Test
+{
+    [TestFixture]
+  public class SolutionTest
+  {
+    [Test]
+    public void SampleTest()
+    {
+      Assert.AreEqual(true, Kata.IsValidWalk(new string[] {"n","s","n","s","n","s","n","s","n","s"}), "should return true");
+      //Assert.AreEqual(false, Kata.IsValidWalk(new string[] {"w","e","w","e","w","e","w","e","w","e","w","e"}), "should return false");
+      //Assert.AreEqual(false, Kata.IsValidWalk(new string[] {"w"}), "should return false");
+      //Assert.AreEqual(false, Kata.IsValidWalk(new string[] {"n","n","n","s","n","s","n","s","n","s"}), "should return false");
+    }
+  }
+}
---
+++ Take a Ten Minute Walk/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Take_a_Ten_Minute_Walk
+{
+    public class Kata
+    {
+        public static bool IsValidWalk(string[] walk)
+        {
+            //insert brilliant code here
+            throw new NotImplementedException();
+        }
+    }
+}
