9024c088722c9b307d93413a798b3e6793d90d47
Mason Cheng <masonsanchen@>
2017-12-26 12:47:07 +0800

    add Get the Middle Character introduction and rename class

---
+++ Get the Middle Character Test/GetMiddleTest.cs
@@ -0,0 +1,16 @@
+﻿using Get_the_Middle_Character;
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class GetMiddleTest
+{
+    [Test]
+    public void GenericTests()
+    {
+        Assert.AreEqual("es", Kata.GetMiddle("test"));
+        Assert.AreEqual("t", Kata.GetMiddle("testing"));
+        Assert.AreEqual("dd", Kata.GetMiddle("middle"));
+        Assert.AreEqual("A", Kata.GetMiddle("A"));
+    }
+}
--- Get the Middle Character/Kata.cs
+++ Get the Middle Character/Kata.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿//You are going to be given a word.Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+//#Examples:
+//runBF("test") should return "es"
+//runBF("testing") should return "t"
+//runBF("middle") should return "dd"
+//runBF("A") should return "A"
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +19,7 @@ namespace Get_the_Middle_Character
         public static string GetMiddle(string s)
         {
             //Code goes here!
+            throw new NotImplementedException();
         }
     }
 }
