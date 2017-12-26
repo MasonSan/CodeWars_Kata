20bd733b820b9b98cca909430cbcebb52304cc5e
Mason Cheng <masonsanchen@>
2017-12-26 12:43:16 +0800

    add Get the Middle Character and test project

---
+++ Get the Middle Character Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Get_the_Middle_Character_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GenericTests()
+        {
+            Assert.AreEqual("es", Kata.GetMiddle("test"));
+            Assert.AreEqual("t", Kata.GetMiddle("testing"));
+            Assert.AreEqual("dd", Kata.GetMiddle("middle"));
+            Assert.AreEqual("A", Kata.GetMiddle("A"));
+        }
+    }
+}
