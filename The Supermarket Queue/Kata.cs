4cea773123e45a2bfe540940e396aaadd5c7ce95
Mason Cheng <masonsanchen@micron.com>
2018-01-02 16:22:37 +0800

    finished The_Supermarket_Queue ny looking best pratice in codewars.
    need to review it later.

--- The Supermarket Queue/Kata.cs
+++ The Supermarket Queue/Kata.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using NUnit.Framework;
 using System.Collections.Generic;
-using NUnit.Framework;
+using System.Linq;
 
 namespace The_Supermarket_Queue
 {
@@ -9,7 +8,7 @@ namespace The_Supermarket_Queue
     public class Kata
     {
         [Test]
-        public void ExampleTest1()
+        public void QueueTime_ExampleTest1()
         {
             long expected = 0;
             long actual = Kata.QueueTime(new int[] { }, 1);
@@ -17,7 +16,7 @@ namespace The_Supermarket_Queue
         }
 
         [Test]
-        public void ExampleTest2()
+        public void QueueTime_ExampleTest2()
         {
             long expected = 10;
             long actual = Kata.QueueTime(new int[] { 1, 2, 3, 4 }, 1);
@@ -25,7 +24,7 @@ namespace The_Supermarket_Queue
         }
 
         [Test]
-        public void ExampleTest3()
+        public void QueueTime_ExampleTest3()
         {
             long expected = 9;
             long actual = Kata.QueueTime(new int[] { 2, 2, 3, 3, 4, 4 }, 2);
@@ -33,7 +32,7 @@ namespace The_Supermarket_Queue
         }
 
         [Test]
-        public void ExampleTest4()
+        public void QueueTime_ExampleTest4()
         {
             long expected = 5;
             long actual = Kata.QueueTime(new int[] { 1, 2, 3, 4, 5 }, 100);
@@ -42,8 +41,23 @@ namespace The_Supermarket_Queue
 
         public static long QueueTime(int[] customers, int n)
         {
+            //var tillArray = new int[n];
 
-            throw new NotImplementedException();
+            //for (var index = 0; index < customers.Length; index++)
+            //{
+            //    Array.Sort(tillArray);
+            //    tillArray[0] += customers[index];
+            //}
+
+            //return tillArray.Max();
+
+            var registers = new List<int>(Enumerable.Repeat(0, n));
+
+            foreach (int cust in customers)
+            {
+                registers[registers.IndexOf(registers.Min())] += cust;
+            }
+            return registers.Max();
         }
     }
 }
