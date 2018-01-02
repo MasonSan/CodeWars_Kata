e37616392334e3093f3607a982c18de068b3d982
Mason Cheng <masonsanchen@micron.com>
2018-01-02 16:29:28 +0800

    add Directions Reduction project.

---
+++ Directions Reduction/DirReduction.cs
@@ -0,0 +1,33 @@
+﻿//Once upon a time, on a way through the old wild west,…
+//… a man was given directions to go from one point to another.The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.Going to one direction and coming back the opposite direction is a needless effort.Since this is the wild west, with dreadfull weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+//How I crossed the desert the smart way.
+
+using System;
+using NUnit.Framework;
+
+namespace Directions_Reduction
+{
+    [TestFixture]
+    public class DirReduction
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+            string[] b = new string[] { "WEST" };
+            Assert.AreEqual(b, DirReduction.dirReduc(a));
+        }
+        [Test]
+        public void Test2()
+        {
+            string[] a = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            string[] b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            Assert.AreEqual(b, DirReduction.dirReduc(a));
+        }
+
+        public static string[] dirReduc(String[] arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
