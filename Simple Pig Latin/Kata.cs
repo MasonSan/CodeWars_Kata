3d729c1ef38ce2b97ccf6cf2924ac92df7531fa0
Mason Cheng <masonsanchen@micron.com>
2018-01-03 11:00:41 +0800

    finish Simple_Pig_Latin

--- Simple Pig Latin/Kata.cs
+++ Simple Pig Latin/Kata.cs
@@ -1,8 +1,8 @@
 ﻿//Move the first letter of each word to the end of it, then add "ay" to the end of the word.Leave punctuation marks untouched.
-using System;
+using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
-using NUnit.Framework;
+using System.Text.RegularExpressions;
 
 namespace Simple_Pig_Latin
 {
@@ -10,15 +10,33 @@ namespace Simple_Pig_Latin
     public class Kata
     {
         [Test]
-        public void KataTests()
+        public void Simple_Pig_Latin_KataTests()
         {
             Assert.AreEqual("igPay atinlay siay oolcay", Kata.PigIt("Pig latin is cool"));
-            Assert.AreEqual("hisTay siay ymay tringsay", Kata.PigIt("This is my string"));
+            Assert.AreEqual("hisTay siay ymay tringsay !", Kata.PigIt("This is my string !"));
         }
 
         public static string PigIt(string str)
         {
-            return str;
+            var result = new List<string>();
+            var addStr = "ay";
+
+            Regex regax = new Regex("[a-zA-Z0-9]+");
+
+            var array = str.Split(' ');
+
+            foreach (var s in array)
+            {
+                if (regax.Match(s).Success)
+                {
+                    var add = s.Substring(1) + s[0] + addStr;
+                    result.Add(string.Join(string.Empty, add));
+                }
+                else
+                    result.Add(s);
+            }
+
+            return string.Join(" ", result);
         }
     }
 }
