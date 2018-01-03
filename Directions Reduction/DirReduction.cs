a5a2c432ab0da686e013eacc1d8970094490c1d1
Mason Cheng <masonsanchen@micron.com>
2018-01-03 10:06:01 +0800

    Finish DirReduction

--- Directions Reduction/DirReduction.cs
+++ Directions Reduction/DirReduction.cs
@@ -2,8 +2,10 @@
 //… a man was given directions to go from one point to another.The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.Going to one direction and coming back the opposite direction is a needless effort.Since this is the wild west, with dreadfull weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
 //How I crossed the desert the smart way.
 
-using System;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Directions_Reduction
 {
@@ -11,14 +13,15 @@ namespace Directions_Reduction
     public class DirReduction
     {
         [Test]
-        public void Test1()
+        public void Directions_Reduction_Test1()
         {
             string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
             string[] b = new string[] { "WEST" };
             Assert.AreEqual(b, DirReduction.dirReduc(a));
         }
+
         [Test]
-        public void Test2()
+        public void Directions_Reduction_Test2()
         {
             string[] a = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
             string[] b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
@@ -27,7 +30,29 @@ namespace Directions_Reduction
 
         public static string[] dirReduc(String[] arr)
         {
-            throw new NotImplementedException();
+            var list = new List<string>(arr);
+            var result = new string[] { };
+            var oppo = new Dictionary<string, string> {  };
+
+            oppo.Add("WEST", "EAST");
+            oppo.Add("NORTH", "SOUTH");
+            oppo.Add("SOUTH", "NORTH");
+            oppo.Add("EAST", "WEST");
+
+            for (var index = 0; index < arr.Length; index++)
+            {
+                if (index + 1 == arr.Length)
+                    break;
+
+                if (arr[index + 1] == oppo[arr[index]])
+                {
+                    list.RemoveAt(index);
+                    list.RemoveAt(index);
+                    arr = dirReduc(list.ToArray());
+                }
+            }
+
+            return arr;
         }
     }
 }
