5ee4e1c3a5b0fd8518e95704bdac4f521ce5118f
Mason Cheng <masonsanchen@micron.com>
2018-01-03 12:43:22 +0800

    Finish Dubstep

--- Dubstep/Dubstep.cs
+++ Dubstep/Dubstep.cs
@@ -1,6 +1,21 @@
-﻿using System;
-using NUnit.Framework;
+﻿//Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance.Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
+
+//Let's assume that a song consists of some number of words. To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
+
+//For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".
+
+//Recently, Jonny has heard Polycarpus's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Polycarpus remixed. Help Jonny restore the original song.
+
+//Input
+//The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
+
+//Output
+//Return the words of the initial song that Polycarpus used to make a dubsteb remix.Separate the words with a space.
+
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 
 namespace Dubstep
 {
@@ -8,7 +23,7 @@ namespace Dubstep
     {
         public static string SongDecoder(string input)
         {
-            // Your code...
+            return String.Join(" ", input.Replace("WUB", " ").Split(' ').Where(s => s != string.Empty));
         }
     }
 
@@ -17,13 +32,13 @@ namespace Dubstep
     public class DubstepTests
     {
         [Test]
-        public void Test1()
+        public void Dubstep_Test1()
         {
             Assert.AreEqual("ABC", Dubstep.SongDecoder("WUBWUBABCWUB"));
         }
 
         [Test]
-        public void Test2()
+        public void Dubstep_Test2()
         {
             Assert.AreEqual("R L", Dubstep.SongDecoder("RWUBWUBWUBLWUB"));
         }
