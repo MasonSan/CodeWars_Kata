4e5903f28eeba35eb348039b60303188ad66a2f2
Mason Cheng <masonsanchen@micron.com>
2018-01-08 10:12:29 +0800

    add IP Validation Project

---
+++ IP Validation/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace IP_Validation
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void TestCases()
+        {
+            Assert.AreEqual(Kata.is_valid_IP("12.255.56.1"), true);
+            Assert.AreEqual(Kata.is_valid_IP(""), false);
+            Assert.AreEqual(Kata.is_valid_IP("abc.def.ghi.jkl"), false);
+            Assert.AreEqual(Kata.is_valid_IP("123.456.789.0"), false);
+            Assert.AreEqual(Kata.is_valid_IP("12.34.56"), false);
+            Assert.AreEqual(Kata.is_valid_IP("12.34.56 .1"), false);
+            Assert.AreEqual(Kata.is_valid_IP("12.34.56.-1"), false);
+            Assert.AreEqual(Kata.is_valid_IP("123.045.067.089"), false);
+        }
+    }
+
+    class Kata
+    {
+        public static bool is_valid_IP(string IpAddres)
+        {
+            //Code here
+
+        }
+    }
+}
